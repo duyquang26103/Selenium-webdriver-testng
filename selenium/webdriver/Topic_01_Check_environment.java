3485c232b2b26e4a84edeb6bd755aece8b5f044c
Quang Vuong <duyquang26103@gmail.com>
2021-09-05 16:05:48 +0700

    Add Class Topic_02_Run_On_Browser

--- selenium/webdriver/Topic_01_Check_environment.java
+++ selenium/webdriver/Topic_01_Check_environment.java
@@ -12,36 +12,36 @@ import org.testng.annotations.Test;
 
 public class Topic_01_Check_environment {
 	WebDriver driver;
-	String projectPath = System.getProperty("user.dir");
+	//String projectPath = System.getProperty("user.dir");
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.guru99.com/");
 	}
 
 	@Test
 	public void TC_01_ValidateCurrentUrl() {
 		// Login Page Url matching
 		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
+		Assert.assertEquals(loginPageUrl, "https://www.guru99.com/");
 	}
 
-	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook - Đăng nhập hoặc đăng ký");
-	}
-
-	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
-	}
+//	@Test
+//	public void TC_02_ValidatePageTitle() {
+//		// Login Page title
+//		String loginPageTitle = driver.getTitle();
+//		Assert.assertEquals(loginPageTitle, "Facebook - Đăng nhập hoặc đăng ký");
+//	}
+//
+//	@Test
+//	public void TC_03_LoginFormDisplayed() {
+//		// Login form displayed
+//		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+//	}
 
 	@AfterClass
 	public void afterClass() {
---
+++ selenium/webdriver/Topic_02_Run_On_Browser.java
@@ -0,0 +1,41 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Run_On_Browser {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	//@Test
+	public void TC_01_Firefox_latest() {
+//		1/ firefox latest: 89
+//		2/ Selenium 3.141.59
+//		3/ TestNG 6.14.3
+//		4/ Gecko Driver
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.guru99.com/");
+		driver.close();
+	
+	}
+	
+	@Test
+	public void TC_01_Firefox_Old() {	
+//		1/ firefox 41.0.2
+//		2/ Selenium 2.53.1
+//		3/ no need TestNG
+//		4/ no need Gecko Driver
+		driver = new FirefoxDriver();
+		driver.get("https://www.guru99.com/");
+		driver.close();
+	
+	}
+}
