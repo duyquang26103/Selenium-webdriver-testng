5373d4bdbd3c5c49ec36abcaada677700b1d3e6d
Quang Vuong <duyquang26103@gmail.com>
2021-09-08 21:45:34 +0700

    Add Topic_06_Web_Element_Command

---
+++ selenium/webdriver/Topic_06_Web_Element_Command.java
@@ -0,0 +1,162 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_06_Web_Element_Command {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	By emailTextboxBy = By.id("mail");
+	By ageOver18RadioBy = By.id("over_18");
+	By educationTextboxBy = By.id("edu");
+	By user5TextboxBy = By.xpath("//h5[contains(.,'Name: User5')]");
+	By job1SelectBy = By.id("job1");
+	By job2SelectBy = By.id("job2");
+	By developmentSelectBy = By.id("development");
+	By slide01SliderBy = By.id("slider-1");
+	By javaSelectedBy = By.id("java");
+
+	By passwordTextboxBy = By.id("password");
+	By disableRadioBy = By.xpath("//label[text()='Radio button is disabled']");
+	By bioTextboxBy = By.id("bio");
+	By job3SelectBy = By.id("job3");
+	By checkDisableCBDBy = By.id("check-disbaled");
+	By slide02SliderBy = By.id("slider-2");
+
+	@BeforeClass
+	public void BeforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+	}
+
+	@Test
+	public void TC_01_isDisplayed() {
+		if (driver.findElement(By.id("mail")).isDisplayed()) {
+			driver.findElement(By.id("mail")).sendKeys("Automation Testing");
+			System.out.println("Email is displayed");
+		} else {
+			System.out.println("Email is not displayed");
+		}
+
+		if (driver.findElement(By.id("under_18")).isDisplayed()) {
+			System.out.println("under_18 is displayed");
+		} else {
+			System.out.println("Email is not displayed");
+		}
+
+		if (driver.findElement(By.id("edu")).isDisplayed()) {
+			driver.findElement(By.id("edu")).sendKeys("Automation Testing");
+			System.out.println("Education is displayed");
+		} else {
+			System.out.println("Email is not displayed");
+		}
+
+		Assert.assertFalse(driver.findElement(By.xpath("//h5[contains(.,'Name: User5')]")).isDisplayed());
+	}
+
+	@Test
+	public void TC_02_isDisplayed_Refactor() {
+		if (isElementDisplayBy(emailTextboxBy)) {
+			sendKeytoElement(emailTextboxBy, "Automation Testing");
+		}
+
+		if (isElementDisplayBy(ageOver18RadioBy)) {
+			clickToElement(ageOver18RadioBy);
+		}
+
+		if (isElementDisplayBy(educationTextboxBy)) {
+			sendKeytoElement(educationTextboxBy, "Automation Testing");
+		}
+
+		Assert.assertFalse(isElementDisplayBy(user5TextboxBy));
+	}
+
+	@Test
+	public void TC_03_isEnabled() {
+		if(isElementEnabledBy(emailTextboxBy));
+		if(isElementEnabledBy(ageOver18RadioBy));
+		if(isElementEnabledBy(educationTextboxBy));
+		if(isElementEnabledBy(job1SelectBy));
+		if(isElementEnabledBy(job2SelectBy));
+		if(isElementEnabledBy(developmentSelectBy));
+		if(isElementEnabledBy(slide01SliderBy));
+		if(isElementEnabledBy(passwordTextboxBy));
+		if(isElementEnabledBy(disableRadioBy));
+		if(isElementEnabledBy(bioTextboxBy));
+		if(isElementEnabledBy(job3SelectBy));
+		if(isElementEnabledBy(slide02SliderBy));
+		if(isElementEnabledBy(checkDisableCBDBy));
+
+	}
+
+	@Test
+	public void TC_04_isSelected() {
+		clickToElement(ageOver18RadioBy);
+		clickToElement(javaSelectedBy);
+		if(isElementSelectedBy(ageOver18RadioBy));
+		if(isElementSelectedBy(javaSelectedBy));
+		clickToElement(javaSelectedBy);
+		if(isElementSelectedBy(javaSelectedBy));
+	}
+
+	public boolean isElementDisplayBy(By by) {
+		WebElement element = driver.findElement(by);
+		if (element.isDisplayed()) {
+			System.out.println("Element" + by + " is displayed");
+			return true;
+		} else {
+			System.out.println("Element" + by + " is not displayed");
+			return false;
+		}
+	}
+
+	public boolean isElementEnabledBy(By by) {
+		WebElement element = driver.findElement(by);
+		if (element.isEnabled()) {
+			System.out.println("Element" + by + " is Enabled");
+			return true;
+		} else {
+			System.out.println("Element" + by + " is not Enabled");
+			return false;
+		}
+	}
+	
+	public boolean isElementSelectedBy(By by) {
+		WebElement element = driver.findElement(by);
+		if (element.isSelected()) {
+			System.out.println("Element" + by + " is Selected");
+			return true;
+		} else {
+			System.out.println("Element" + by + " is not Selected");
+			return false;
+		}
+	}
+
+	public void sendKeytoElement(By by, String value) {
+		WebElement element = driver.findElement(by);
+		element.clear();
+		element.sendKeys(value);
+	}
+
+	public void clickToElement(By by) {
+		WebElement element = driver.findElement(by);
+		element.click();
+	}
+
+	@AfterClass
+	public void AfterClass() {
+		//driver.quit();
+	}
+}
