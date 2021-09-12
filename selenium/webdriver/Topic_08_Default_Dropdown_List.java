dc8bf1bddfcc0b5e81e459ee7b9bd0c21005bafa
Quang Vuong <duyquang26103@gmail.com>
2021-09-12 11:15:39 +0700

    Update Topic_08 Done Excersice TC_03 HTML Dropdown/List

--- selenium/webdriver/Topic_08_Default_Dropdown_List.java
+++ selenium/webdriver/Topic_08_Default_Dropdown_List.java
@@ -1,7 +1,7 @@
 package webdriver;
 
-import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,6 +19,22 @@ public class Topic_08_Default_Dropdown_List {
 	String projectPath = System.getProperty("user.dir");
 	
 	Select select;
+	
+	//testcase 2
+	String fName, lName, day, month, year, email, companyName, password, cPassword;
+	By genderRadioBy = By.id("gender-male");
+	By fNameTextboxBy = By.id("FirstName");
+	By lNameTextboxBy = By.id("LastName");
+	By dayBy = By.name("DateOfBirthDay");
+	By monthBy = By.name("DateOfBirthMonth");
+	By yearBy = By.name("DateOfBirthYear");
+	By emailTextboxBy = By.id("Email");
+	By companyNameTextboxBy = By.id("Company");
+	By passworTextboxdBy = By.id("Password");
+	By cPassworTextboxdBy = By.id("ConfirmPassword");
+	By registerButtonBy = By.id("register-button");
+	
+	
 
 	@BeforeClass
 	public void BeforeClass() {
@@ -26,10 +42,13 @@ public class Topic_08_Default_Dropdown_List {
 		driver = new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		email = "shogim" + randInt() + "@gmail.com";
 	}
 
-	@Test
+	//@Test
 	public void TC_01_Rode() {
+
 		driver.get("https://www.rode.com/wheretobuy");
 		
 		select = new Select(driver.findElement(By.name("where_country")));
@@ -51,9 +70,68 @@ public class Topic_08_Default_Dropdown_List {
 		}
 		
 	}
+	
+	@Test
+	public void TC_02_Ecommerce() {
+		driver.get("https://demo.nopcommerce.com/");
+		driver.findElement(By.cssSelector(".ico-register")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='Register']")).isDisplayed());
+		
+		driver.findElement(genderRadioBy).click();
+		
+		select = new Select(driver.findElement(dayBy));
+		select.selectByVisibleText("1");
+		List<WebElement> dateOfBirthDay = driver.findElements(By.xpath("//select[@name='DateOfBirthDay']/option"));
+		Assert.assertEquals(dateOfBirthDay.size(),32);
+		
+		select = new Select(driver.findElement(monthBy));
+		select.selectByVisibleText("May");
+		List<WebElement> dateOfBirthMonth = driver.findElements(By.xpath("//select[@name='DateOfBirthMonth']/option"));
+		Assert.assertEquals(dateOfBirthMonth.size(),13);
+		
+		select = new Select(driver.findElement(yearBy));
+		select.selectByVisibleText("1999");
+		List<WebElement> dateOfBirthYear = driver.findElements(By.xpath("//select[@name='DateOfBirthYear']/option"));
+		Assert.assertEquals(dateOfBirthYear.size(),112);
+		
+		driver.findElement(fNameTextboxBy).sendKeys("carlot");
+		driver.findElement(lNameTextboxBy).sendKeys("miro");
+		
+		driver.findElement(emailTextboxBy).sendKeys(email);
+		driver.findElement(companyNameTextboxBy).sendKeys("sabialonso TDA");
+		driver.findElement(passworTextboxdBy).sendKeys("123123");
+		driver.findElement(cPassworTextboxdBy).sendKeys("123123");
+		
+		driver.findElement(registerButtonBy).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='result']")).isDisplayed());
+		
+		driver.findElement(By.xpath("//a[@class='button-1 register-continue-button']")).click();
+		
+		driver.findElement(By.xpath("//a[@class='ico-account']")).click();
+		
+		Assert.assertEquals(driver.findElement(genderRadioBy).getAttribute("value"), "M");
 
+		
+		Assert.assertEquals(driver.findElement(fNameTextboxBy).getAttribute("value"), "carlot");
+		Assert.assertEquals(driver.findElement(lNameTextboxBy).getAttribute("value"), "miro");
+		Assert.assertEquals(driver.findElement(emailTextboxBy).getAttribute("value"), email);
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']/option[text()='1']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']/option[text()='May']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//select[@name='DateOfBirthYear']/option[text()='1999']")).isDisplayed());
+
+		
+		Assert.assertEquals(driver.findElement(companyNameTextboxBy).getAttribute("value"), "sabialonso TDA");
+	}
+	
 	@AfterClass
 	public void AfterClass() {
 		driver.quit();
+	}
+	
+	public int randInt() {
+		Random rand = new Random();
+		return rand.nextInt(999);
 	}
 }
